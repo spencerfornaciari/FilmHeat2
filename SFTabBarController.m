aea88e6356bc6d026343bf939784d757962205d7
Spencer Fornaciari <mail@spencerfornaciari.com>
2014-01-20 15:06:32 -0800

    Storyboarding of Film Heat

---
+++ SFTabBarController.m
@@ -0,0 +1,38 @@
+//
+//  SFTabBarController.m
+//  FilmHeat
+//
+//  Created by Spencer Fornaciari on 1/20/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import "SFTabBarController.h"
+
+@interface SFTabBarController ()
+
+@end
+
+@implementation SFTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
